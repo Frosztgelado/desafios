fc17fe4e4b013bc4d5806f10bda9d09cbf16f92f
Frosztgelado <136204920+Frosztgelado@users.noreply.github.com>
2023-08-08 13:32:43 -0300

    correções e adições

--- scrpts/calculadora/calcular.js
+++ scrpts/calculadora/calcular.js
@@ -1,40 +1,45 @@
-function Calculate() {
-  let valor1 = document.getElementById("valor1");
-  let valor2 = document.getElementById("valor2");
-  let val1 = Number(valor1.value);
-  let val2 = Number(valor2.value);
-  let showResult = document.getElementById("resultado");
+class Calculadora {
+  constructor() {
+    this.valor1 = document.getElementById("valor1");
+    this.valor2 = document.getElementById("valor2");
+    this.showResult = document.getElementById("resultado");
+  }
 
-  if (val1 == 0 || val2 == 0) {
-    window.alert("Insira valores pra realizar o calculo");
-  } else {
-    let value = GetOperation();
-    let result = 0;
+  Calculate() {
+    let val1 = Number(this.valor1.value);
+    let val2 = Number(this.valor2.value);
 
-    switch (value) {
-      case "adic":
-        result = val1 + val2;
-        showResult.innerHTML = `O resultado da adição foi <b>${result}<b>.`;
-        break;
-      case "subt":
-        result = val1 - val2;
-        showResult.innerHTML = `O resultado da adição foi <b>${result}<b>.`;
-        break;
-      case "mult":
-        result = val1 * val2;
-        showResult.innerHTML = `O resultado da adição foi <b>${result}<b>.`;
-        break;
-      case "divi":
-        result = val1 / val2;
-        showResult.innerHTML = `O resultado da adição foi <b>${result}<b>.`;
-        break;
+    if (val1 == 0 || val2 == 0) {
+      window.alert("Insira valores pra realizar o calculo");
+    } else {
+      let value = this._GetOperation();
+      let result = 0;
+
+      switch (value) {
+        case "adic":
+          result = val1 + val2;
+          this.showResult.innerHTML = `O resultado da adição foi <b>${result}<b>.`;
+          break;
+        case "subt":
+          result = val1 - val2;
+          this.showResult.innerHTML = `O resultado da subtração foi <b>${result}<b>.`;
+          break;
+        case "mult":
+          result = val1 * val2;
+          this.showResult.innerHTML = `O resultado da multiplicação foi <b>${result}<b>.`;
+          break;
+        case "divi":
+          result = val1 / val2;
+          this.showResult.innerHTML = `O resultado da divisão foi <b>${result}<b>.`;
+          break;
+      }
     }
   }
-}
 
-function GetOperation() {
-  let operacao = document.getElementById("operacao");
-  let valor = operacao.value;
+  _GetOperation() {
+    let operacao = document.getElementById("operacao");
+    let valor = operacao.value;
 
-  return valor;
+    return valor;
+  }
 }
